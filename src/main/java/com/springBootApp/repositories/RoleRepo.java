a2f48d2a1c600d68c069b12f77c046565c6b945f
billy.wong <billy.wong@tradelink.com.hk>
2023-02-23 15:22:24 +0800

    first commit

--- src/main/java/com/springBootApp/repositories/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springBootApp.repositories;
-
-import com.springBootApp.models.Role;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepo extends BaseRepo<Role, Long> {
-
-}
--- src/main/java/com/springBootApp/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.springBootApp.repositories;
-
-import com.springBootApp.models.User;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends BaseRepo<User, Long> {
-}
